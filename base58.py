423e95555223703112e616d7d9ae79e69b28ee11
Sam Morris <sam@robots.org.uk>
2012-09-17 18:02:26 +0100

    Add base58 tests
    Test vectors floating around on the Internet are often for the Flickr
    base 58 encoding; this encoding differs from the Bitcoin one, so they
    can not be used.

--- base58.py
+++ base58.py
@@ -2,6 +2,27 @@ _base58_codestring = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxy
 _base58_codestring_len = len (_base58_codestring)
 
 def encode (x):
+        '''
+        Encode bytes into base58-encoded bytes.
+
+        >>> import binascii
+        >>> encode (b'\x30\x39')
+        b'4fr'
+        >>> encode (int (12345).to_bytes (2, 'big'))
+        b'4fr'
+        >>> encode (int (3471391110).to_bytes (4, 'big'))
+        b'6Hknds'
+        >>> encode (binascii.unhexlify (b'00'))
+        b'1'
+        >>> encode (binascii.unhexlify (b'0000'))
+        b'11'
+        >>> encode (binascii.unhexlify (b'01'))
+        b'2'
+        >>> encode (binascii.unhexlify (b'0198b9a10f'))
+        b'BSxJKp'
+        >>> encode (int (3429289555).to_bytes (4, 'big'))
+        b'6E31Jz'
+        '''
         q = int.from_bytes (x, 'big')
         result = bytearray ()
         while q > 0:
